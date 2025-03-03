54e86ea5ea5ba58049fb77f1c3bf810af4be3552
Cabdinasir64 <cabdinasira64@gmail.com>
2025-03-03 16:55:11 +0300

    Second commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundColor:{
+        primary: '#494949'
+      },
+      colors:{
+        nav:'FF8F17'
+      }
+    },
+  },
+  plugins: [],
+}
